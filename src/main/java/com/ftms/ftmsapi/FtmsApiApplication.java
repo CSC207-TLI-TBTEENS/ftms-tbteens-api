050d2ec5365ee36d369bd12ae3cb36c34de4e7a1
Muhammad Khattak <muhammadskhattak@gmail.com>
2018-11-12 20:27:49 +0000

    Added oauth and mysql support. NOTE: Each user will need to add user credentials for mysql in the application.properties file. Backed should run once the user credentials are added.

--- src/main/java/com/ftms/ftmsapi/FtmsApiApplication.java
+++ src/main/java/com/ftms/ftmsapi/FtmsApiApplication.java
@@ -2,8 +2,11 @@ package com.ftms.ftmsapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@EnableOAuth2Sso
 public class FtmsApiApplication {
 
 	public static void main(String[] args) {
