ef4f457d96f1720554ba49694dbb248dbd452221
Muhammad Khattak <muhammadskhattak@gmail.com>
2018-11-17 15:34:18 +0000

    Implemented one-to-one relations with employee and company as well and one-to-many relation with time

---
+++ src/main/java/com/ftms/ftmsapi/model/Task.java
@@ -0,0 +1,65 @@
+package com.ftms.ftmsapi.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.io.Serializable;
+import java.util.Set;
+
+@Entity
+@Table(name="task")
+public class Task implements Serializable{
+    // INSTANCE FIELDS
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(fetch=FetchType.LAZY)
+    @MapsId
+    private Employee employee;
+
+    @OneToOne(fetch=FetchType.LAZY)
+    @MapsId
+    private Job job;
+
+    @OneToMany(mappedBy = "task", cascade = CascadeType.ALL)
+    private Set<Time> times;
+
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    // GETTERS/SETTERS
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    public Job getJob() {
+        return job;
+    }
+
+    public void setJob(Job job) {
+        this.job = job;
+    }
+
+    public Set<Time> getTimes() {
+        return times;
+    }
+
+    public void setTimes(Set<Time> times) {
+        this.times = times;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
