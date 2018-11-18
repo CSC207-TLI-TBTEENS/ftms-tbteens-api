ae450b08595f837ad047ee2e665bc6ba0668370c
WilliamG240 <williamnono94@hotmail.com>
2018-11-18 18:28:15 -0500

    Modification database related

--- src/main/java/com/ftms/ftmsapi/controller/JobController.java
+++ src/main/java/com/ftms/ftmsapi/controller/JobController.java
@@ -1,9 +1,13 @@
 package com.ftms.ftmsapi.controller;
 
+import com.ftms.ftmsapi.model.Job;
+import com.ftms.ftmsapi.model.Timesheet;
 import com.ftms.ftmsapi.repository.JobRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -11,4 +15,12 @@ public class JobController {
 
     @Autowired
     JobRepository jobRepository;
+
+
+    @GetMapping("/jobs")
+    public List<Job> getAllJobs() {
+        return jobRepository.findAll();
+    }
+    
+
 }
