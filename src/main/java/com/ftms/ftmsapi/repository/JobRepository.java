e6cbb6e494e7440fe728145d7d275b0cc252e1b5
Tony Nguyen <anhhao1999@gmail.com>
2018-11-18 14:09:32 -0500

    Delete some conflicts

--- src/main/java/com/ftms/ftmsapi/repository/JobRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ftms.ftmsapi.repository;
-import com.ftms.ftmsapi.model.Job;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface JobRepository extends JpaRepository <Job, Long> {
-}
