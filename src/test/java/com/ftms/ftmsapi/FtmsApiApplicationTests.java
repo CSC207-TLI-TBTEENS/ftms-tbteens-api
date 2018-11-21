e02faf62bdbc9e3d5ced6673be89b6f8c6074df5
ignaspa <ignaspan@gmail.com>
2018-11-21 18:42:26 -0500

    remove test annotation as a test

--- src/test/java/com/ftms/ftmsapi/FtmsApiApplicationTests.java
+++ src/test/java/com/ftms/ftmsapi/FtmsApiApplicationTests.java
@@ -5,10 +5,10 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
 public class FtmsApiApplicationTests {
-	
+	//@Test
 	public void contextLoads() {
 	}
 }
