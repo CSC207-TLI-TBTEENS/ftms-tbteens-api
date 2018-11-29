8d891f6876d51a8f3f8dfd804bd3622e6f175472
ruth.crasto <ruth.crasto@mail.utoronto.ca>
2018-11-28 19:22:43 -0500

    Added SQL queries for jobs and timesheets.

--- src/main/java/com/ftms/ftmsapi/repository/JobRepository.java
+++ src/main/java/com/ftms/ftmsapi/repository/JobRepository.java
@@ -1,9 +1,20 @@
 package com.ftms.ftmsapi.repository;
 import com.ftms.ftmsapi.model.Job;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface JobRepository extends JpaRepository<Job, Long> {
+
+
+    // SQL queries
+    String FIND_JOBS_FROM_TIMESHEET_ID = "SELECT * FROM jobs WHERE timesheet_id = ?1";
+
+
+    @Query(value = FIND_JOBS_FROM_TIMESHEET_ID, nativeQuery = true)
+    public List<Job> findJobsFromTimesheetId(int timesheet_id);
 
 }
