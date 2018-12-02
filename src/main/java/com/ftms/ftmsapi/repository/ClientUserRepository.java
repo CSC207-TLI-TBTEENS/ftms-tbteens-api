43b5a7b5acb110ad59b78d86fa2cf0bee81f81ab
u2agarwal99 <u2agarwal99@gmail.com>
2018-12-02 10:04:23 -0500

    Delete ClientUserRepository.java
    Not needed, User Repository is generic class that accepts all children of Users

--- src/main/java/com/ftms/ftmsapi/repository/ClientUserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ftms.ftmsapi.repository;
-
-import com.ftms.ftmsapi.model.ClientUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ClientUserRepository extends JpaRepository<ClientUser, Long> {
-}
