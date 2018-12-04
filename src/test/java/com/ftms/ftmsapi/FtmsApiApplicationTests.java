47d00698b78e5976fb32f4e930dc978fbbb88276
Ece Yucer <eceyucer@gmail.com>
2018-12-04 14:24:53 -0500

    Fixed UserSummary.

--- src/test/java/com/ftms/ftmsapi/FtmsApiApplicationTests.java
+++ src/test/java/com/ftms/ftmsapi/FtmsApiApplicationTests.java
@@ -4,14 +4,15 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
+/*
 @RunWith(SpringRunner.class)
 @SpringBootTest
+*/
 public class FtmsApiApplicationTests {
-
+    /*
 	@Test
 	public void contextLoads() throws Exception {
 	}
 
-
+    */
 }
