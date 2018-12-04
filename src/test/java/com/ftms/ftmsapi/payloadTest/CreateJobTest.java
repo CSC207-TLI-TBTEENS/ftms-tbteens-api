1176797e1358e0beb24e90d54e1f52fd15662f8f
Ece Yucer <eceyucer@gmail.com>
2018-12-04 14:01:30 -0500

    Added the new testing classes.

---
+++ src/test/java/com/ftms/ftmsapi/payloadTest/CreateJobTest.java
@@ -0,0 +1,32 @@
+package com.ftms.ftmsapi.payloadTest;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CreateJobTest {
+
+    @Test
+    public void getJobTitle() {
+    }
+
+    @Test
+    public void setJobTitle() {
+    }
+
+    @Test
+    public void getDescription() {
+    }
+
+    @Test
+    public void setDescription() {
+    }
+
+    @Test
+    public void getSiteName() {
+    }
+
+    @Test
+    public void setSiteName() {
+    }
+}
